1c4c9c2820814a22788a661df8ba0a215af57af1
nurulamin092 <mnurulamin092@gmial.com>
2022-03-10 14:57:22 +0600

    card add

--- script.js
+++ script.js
@@ -14,10 +14,28 @@ const handleSearch = () => {
 }
 
 const showArtist = ({ artists }) => {
-  console.log(artists);
+
+  const artistContainer = elementById('artists');
   // const artists = data.artists
   // const { artists } = data;
   artists.forEach(artist => {
+    const div = document.createElement('div');
+    div.classList.add('artist-card')
+    div.innerHTML = `<div class="image-container">
+    <div class="image-container-inner">
+      <img src="" alt="" />
+    </div>
+  </div>
+  <div class="info-container">
+    <h1></h1>
+    <p>Country:</p>
+    <p>Style:</p>
+  </div>
+  <button class="album-button">
+    <i class="fa-solid fa-compact-disc"></i>
+    <p class="button-title">Albums</p>
+  </button></p>`
     console.log(artist);
+    artistContainer.appendChild(div)
   });
 }
